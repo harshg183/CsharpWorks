e89c01d12328e529e4fa38cde4d353ddb2a72a81
harshg183 <harshgupta1253@gmail.com>
2025-06-19 18:22:27 +0530

    Add project files.

---
+++ DemoSolution19june/DemoProject19june/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject19june
+{
+    class Program
+    {
+        int i;bool b;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Value of i is: "+p.i);
+            Console.WriteLine("value of b is: "+p.b);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstSolution/FirstProject/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Class1
+    {
+        static void  Main()
+        {
+            Console.WriteLine("Second class in second project");
+        }
+    }
+}
